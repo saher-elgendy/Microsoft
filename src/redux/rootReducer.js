2735cc21d905d79aa6b17976254acf9d54b209a2
unknown <saherelgendy1990@gmail.com>
2019-11-12 02:55:41 -0800

    adding cartReducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
+import cartReducer from './cart/reducer';
 import filtersReducer from './filters/reducer';
 import productsReducer from './products/reducer';
 import sortReducer from './sort/reducer';
@@ -7,5 +8,6 @@ import sortReducer from './sort/reducer';
 export default combineReducers({
     products: productsReducer,
     filters: filtersReducer,
-    sortBy: sortReducer
+    sortBy: sortReducer,
+    cartProducts: cartReducer
 });
