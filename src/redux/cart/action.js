a1661e9d22d497e395c392850eb912fbcf6ff52e
unknown <saherelgendy1990@gmail.com>
2019-11-12 17:04:26 -0800

    refactoring updateCart action

--- src/redux/cart/action.js
+++ src/redux/cart/action.js
@@ -6,26 +6,28 @@ import { UPDATE_CART } from './actionTypes';
  * @param {number} quantity 1 by default
  */
 
-export const updateCart = (cartProducts, productToAdd, quantity) => {
+export const updateCart = (productToAdd, cartProducts, quantity) => {
+    console.log(productToAdd)
     const newCartProducts = [...cartProducts];
-    //products are initially added with quantity set to 1
-    if (productToAdd.quantity) {
-        productToAdd.quantity = quantity || 1;
-    }
-    //find if the product to add already exists in the cart
+    //check if the product to add already exists in the cart
     const repeatedProductIndex = newCartProducts.findIndex(p => p.id === productToAdd.id);
     const repeatedProduct = newCartProducts[repeatedProductIndex];
-    //if the product exist increment the quantity by 1 or any quantity user choose
+    //if the product exists, increment the quantity by 1 or any quantity user choose
     if (repeatedProduct) {
-       newCartProducts[repeatedProductIndex] = {
-           ...repeatedProduct,
-           quantity: repeatedProduct.quantity + (quantity || 1)
-       }
+        newCartProducts[repeatedProductIndex] = {
+            ...repeatedProduct,
+            quantity: repeatedProduct.quantity + (quantity || 1)
+        }
     }
     //if the product not exist in the cart, push it
     else {
-        newCartProducts.push(productToAdd);
+        newCartProducts.push({
+            ...productToAdd,
+            quantity: 1  
+        });
     }
+
+    console.log(newCartProducts)
     //the action
     return {
         type: UPDATE_CART,
