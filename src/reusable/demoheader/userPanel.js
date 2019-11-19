97b9ac106a00f848b4b3c017b6df68a7e1231f9d
unknown <saherelgendy1990@gmail.com>
2019-11-18 21:53:52 -0800

    adding closing cart functionality

--- src/reusable/demoheader/userPanel.js
+++ src/reusable/demoheader/userPanel.js
@@ -7,24 +7,26 @@ const UserPanel = (props) => {
 
     return (
         <div className="user-panel">
-            {cartOpen ? <Cart classes={{ cart: 'cart-open' }} /> : ''}
+            <Cart classes={{ cart: cartOpen ? 'cart-open' : 'cart-closed' }} />
+            <div className="user-panel-items">
+                <ul className="user-panel-icons d-flex justify-content-between">
+                    <li className="user-panel-icon" >
+                        <i
+                            onClick={handleToggleCartShow}
+                            className="fas fa-shopping-cart"
+                            aria-hidden="true">
+                        </i>
+                        <span className="cart-products-num text-primary">{cartProducts.length}</span>
+                    </li>
+                    <li className="user-panel-icon">
+                        <i className="fas fa-search" aria-hidden="true"></i>
+                    </li>
+                    <li className="user-panel-icon">
+                        <i className="fas fa-user" aria-hidden="true"></i>
+                    </li>
+                </ul>
+            </div>
 
-            <ul className="user-panel-icons d-flex">
-                <li className="user-panel-icon" >
-                    <i
-                        onClick={handleToggleCartShow}
-                        className="fas fa-shopping-cart"
-                        aria-hidden="true">
-                    </i>
-                    <span className="cart-products-num text-primary">{cartProducts.length}</span>
-                </li>
-                <li className="user-panel-icon">
-                    <i className="fas fa-search" aria-hidden="true"></i>
-                </li>
-                <li className="user-panel-icon">
-                    <i className="fas fa-user" aria-hidden="true"></i>
-                </li>
-            </ul>
         </div>
     );
 }
