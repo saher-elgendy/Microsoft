14f9d51aebd638b2d8a373ca941f8741513c10be
unknown <saherelgendy1990@gmail.com>
2020-09-17 22:05:53 -0700

    Updates

---
+++ precache-manifest.923afb33c697356d75057c95e481c149.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "762cbf9952c6c00e6a8de9a6023a950f",
+    "url": "/Microsoft/index.html"
+  },
+  {
+    "revision": "a994f6e79ebc6e858f25",
+    "url": "/Microsoft/static/css/1.d3211762.chunk.css"
+  },
+  {
+    "revision": "26e2ee073e46b3006b6f",
+    "url": "/Microsoft/static/css/4.8e1873ca.chunk.css"
+  },
+  {
+    "revision": "14adbcdedb1cf2d013ee",
+    "url": "/Microsoft/static/css/5.ec6c63a3.chunk.css"
+  },
+  {
+    "revision": "25e52260b71be902ae63",
+    "url": "/Microsoft/static/css/6.cf8945eb.chunk.css"
+  },
+  {
+    "revision": "86a0af668e8452ee5275",
+    "url": "/Microsoft/static/css/7.6dfd5f0a.chunk.css"
+  },
+  {
+    "revision": "e9a247f279c4969bcdbe",
+    "url": "/Microsoft/static/css/8.f3ab6611.chunk.css"
+  },
+  {
+    "revision": "9563e72194185c27ab36",
+    "url": "/Microsoft/static/css/9.7ff1f38d.chunk.css"
+  },
+  {
+    "revision": "2a09f2523d816e17709a",
+    "url": "/Microsoft/static/css/main.f0727b60.chunk.css"
+  },
+  {
+    "revision": "4551a029d4945582479f",
+    "url": "/Microsoft/static/js/0.319854ec.chunk.js"
+  },
+  {
+    "revision": "488ce6dc1f768f05cdbd71ff2f9913cd",
+    "url": "/Microsoft/static/js/0.319854ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a994f6e79ebc6e858f25",
+    "url": "/Microsoft/static/js/1.79dfefbb.chunk.js"
+  },
+  {
+    "revision": "99bbc9e9c8cdb09611b9",
+    "url": "/Microsoft/static/js/10.937d05ad.chunk.js"
+  },
+  {
+    "revision": "26e2ee073e46b3006b6f",
+    "url": "/Microsoft/static/js/4.041e793f.chunk.js"
+  },
+  {
+    "revision": "bbca33d598126f7490735d6383acbdf6",
+    "url": "/Microsoft/static/js/4.041e793f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14adbcdedb1cf2d013ee",
+    "url": "/Microsoft/static/js/5.8fa3b779.chunk.js"
+  },
+  {
+    "revision": "25e52260b71be902ae63",
+    "url": "/Microsoft/static/js/6.797bac13.chunk.js"
+  },
+  {
+    "revision": "86a0af668e8452ee5275",
+    "url": "/Microsoft/static/js/7.9ab83840.chunk.js"
+  },
+  {
+    "revision": "e9a247f279c4969bcdbe",
+    "url": "/Microsoft/static/js/8.efa85843.chunk.js"
+  },
+  {
+    "revision": "9563e72194185c27ab36",
+    "url": "/Microsoft/static/js/9.5f43950b.chunk.js"
+  },
+  {
+    "revision": "2a09f2523d816e17709a",
+    "url": "/Microsoft/static/js/main.0c35a11d.chunk.js"
+  },
+  {
+    "revision": "326fc1fe60837534d77e",
+    "url": "/Microsoft/static/js/runtime-main.571a43cf.js"
+  },
+  {
+    "revision": "25f7b662981770fae231c66403bee584",
+    "url": "/Microsoft/static/media/best_sale.25f7b662.png"
+  },
+  {
+    "revision": "5fded0a2c080599775b824a6f03968b9",
+    "url": "/Microsoft/static/media/correct.5fded0a2.png"
+  },
+  {
+    "revision": "1d4d4e3a82500f04e9300770188d229a",
+    "url": "/Microsoft/static/media/laptop.1d4d4e3a.png"
+  },
+  {
+    "revision": "42dfd44bc01d55286643137e25207bd4",
+    "url": "/Microsoft/static/media/microsoft.42dfd44b.png"
+  },
+  {
+    "revision": "d3b1269ca0db3a95f1b634e5333ac35a",
+    "url": "/Microsoft/static/media/mobile.d3b1269c.png"
+  },
+  {
+    "revision": "3eeba7db378fda3abe1ab4396a6a8b5a",
+    "url": "/Microsoft/static/media/mouse.3eeba7db.png"
+  },
+  {
+    "revision": "28e090f3b1efde5744e23fa9f3ca09f9",
+    "url": "/Microsoft/static/media/panorama.28e090f3.png"
+  },
+  {
+    "revision": "1368f96dcb91320c1e691ac504ae3a66",
+    "url": "/Microsoft/static/media/surface_laptop.1368f96d.png"
+  },
+  {
+    "revision": "330c36f648ea1fcae8c265bc626f0536",
+    "url": "/Microsoft/static/media/surface_laptop2.330c36f6.png"
+  },
+  {
+    "revision": "307ac3106f0303ade77e32a567c157eb",
+    "url": "/Microsoft/static/media/tablet1.307ac310.png"
+  },
+  {
+    "revision": "8764266de127bcfe97b4620f13fce8c4",
+    "url": "/Microsoft/static/media/tablet2.8764266d.png"
+  },
+  {
+    "revision": "b8ad169d312999ed4e060e89955d71cd",
+    "url": "/Microsoft/static/media/visual_studio.b8ad169d.png"
+  }
+]);
